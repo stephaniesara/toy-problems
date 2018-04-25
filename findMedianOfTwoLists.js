7f1a900c3e1e80ff7cd81bcfc2884f016421e72d
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-04-24 21:41:38 -0700

    Pass tests

--- findMedianOfTwoLists.js
+++ findMedianOfTwoLists.js
@@ -1,33 +1,41 @@
 const findMedianOfTwoLists = (listA, listB) => {
-  console.log("looping");
-  console.log(listA, listB);
   const n = listA.length;
-  const getAverage = (a, b) => {
-    return (a + b) / 2;
-  };
+
+  // helper func to calculate avg of two nums
+  const getAverage = (a, b) => (a + b) / 2;
+
+  // edge case, n < 2
   if (n === 1) {
     return getAverage(listA[0], listB[0]);
   }
+
+  // base case, n is 2 -> concat the two lists and return median of those 4 numbers
   if (n === 2) {
     const combined = listA.concat(listB).sort();
     return getAverage(combined[1], combined[2]);
   }
+
+  // get elements at midpoints
   const mid = Math.floor(n / 2);
   let elemA = listA[mid];
   let elemB = listB[mid];
+
+  // special case -> the midpoint nums are the medians, return their average
   if (elemA === elemB) {
-    return (elemA + elemB) / 2;
+    return getAverage(elemA, elemB);
   }
-  const adjustedMidForOdd = n % 2 ? mid : mid - 1;
+
+  // recursively search opposite halves of the two lists
+  const adjustedMid = n % 2 ? mid : mid - 1; // to account for n is even
   if (elemA < elemB) {
     return findMedianOfTwoLists(
-      listA.slice(adjustedMidForOdd),
+      listA.slice(adjustedMid),
       listB.slice(0, mid + 1)
     );
   } else {
     return findMedianOfTwoLists(
       listA.slice(0, mid + 1),
-      listB.slice(adjustedMidForOdd)
+      listB.slice(adjustedMid)
     );
   }
 };
@@ -38,67 +46,32 @@ const expect = (actual, expected) => {
   console.log(actual === expected, "expected", actual, "to be", expected);
 };
 
+// n < 2, edge case
 var listA = [1];
 var listB = [3];
 expect(findMedianOfTwoLists(listA, listB), 2);
 
+// n is odd, all numbers in one list are smaller than all nums in other
 listA = [1, 2, 3];
 listB = [4, 5, 6];
 expect(findMedianOfTwoLists(listA, listB), 3.5);
 
+// n is odd
 listA = [1, 5, 9, 100, 101];
 listB = [3, 6, 7, 8, 11];
 expect(findMedianOfTwoLists(listA, listB), 7.5);
 
+// n is even
 listA = [1, 5, 9, 100];
 listB = [3, 6, 7, 8];
 expect(findMedianOfTwoLists(listA, listB), 6.5);
 
+// n is even, the lists are reversed from previous test
 listA = [3, 6, 7, 8];
 listB = [1, 5, 9, 100];
 expect(findMedianOfTwoLists(listA, listB), 6.5);
 
-// const findMedianOfTwoLists = (listA, listB) => {
-//   const n = listA.length;
-//   const mid = Math.floor(n / 2); // odd
-//   let currB = mid;
-//   let currA = mid;
-//   let prevA = mid;
-//   let prevB = mid;
-
-//   const isAtEndOfList = index => {
-//     return index === 0 || index === n - 1;
-//   };
-
-//   while (true) {
-//     console.log("looping");
-//     console.log(listA[prevA], listB[prevB]);
-//     console.log(listA[currA], listB[currB]);
-//     if (listA[currA] === listB[currB] || isAtEndOfList(currA)) {
-//       break;
-//     }
-//     if (listA[currA] > listB[currB]) {
-//       if (listA[prevA] < listB[prevB]) {
-//         currA = prevA;
-//         currB = prevB;
-//         break;
-//       }
-//       prevA = currA;
-//       prevB = currB;
-//       currA--;
-//       currB++;
-//     } else {
-//       if (listA[prevA] > listB[prevB]) {
-//         currA = prevA;
-//         currB = prevB;
-//         break;
-//       }
-//       prevA = currA;
-//       prevB = currB;
-//       currA++;
-//       currB--;
-//     }
-//   }
-
-//   return (listA[currA] + listB[currB]) / 2;
-// };
+// n is even, median is duplicated numbers
+listA = [1, 3, 4, 6];
+listB = [6, 7, 100, 1000];
+expect(findMedianOfTwoLists(listA, listB), 6);
