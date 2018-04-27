6197886994b99228d613747deb09fd231514db7c
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-04-27 16:52:29 -0700

    Implement isUnique with O(n log n) time

---
+++ isUnique.js
@@ -0,0 +1,50 @@
+// CTCI 90
+// Implement an algo to determine if a string has all unique chars.
+// What if you cannot use addtl data structures?
+
+const isUnique = str => {
+  if (str.length < 2) {
+    return true;
+  }
+  let sorted = str.split("").sort(); // O(n log n)
+  let prev = sorted[0];
+  for (let i = 1; i < str.length; i++) {
+    // O(n)
+    let curr = sorted[i];
+    if (curr === prev) {
+      return false;
+    }
+    prev = curr;
+  }
+  return true;
+};
+
+// TESTS ------------------------
+
+const expect = (actual, expected) => {
+  console.log(actual === expected, "expected", actual, "to be", expected);
+};
+
+let input = "hello";
+let expected = false;
+expect(isUnique(input), expected);
+
+input = "a";
+expected = true;
+expect(isUnique(input), expected);
+
+input = "aa";
+expected = false;
+expect(isUnique(input), expected);
+
+input = "helllo";
+expected = false;
+expect(isUnique(input), expected);
+
+input = "";
+expected = true;
+expect(isUnique(input), expected);
+
+input = "abacadaba";
+expected = false;
+expect(isUnique(input), expected);
