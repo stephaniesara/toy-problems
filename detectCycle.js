0e4d9b9b1e9b877fa797af6cbf9d6885fe0e2b7c
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-05-24 10:37:25 -0700

    Implement detectCycle

---
+++ detectCycle.js
@@ -0,0 +1,30 @@
+// Leetcode
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      let restart = head;
+      while (head !== fast) {
+        head = head.next;
+        fast = fast.next;
+      }
+      return head;
+    }
+  }
+  return null;
+};
