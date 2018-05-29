ac4d131c1ee94cadfa85dd0b0d425cd5a8ea5bb2
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-05-29 09:56:43 -0700

    Implement partition while adding to head and tail

--- partition-linkedlists.js
+++ partition-linkedlists.js
@@ -16,25 +16,45 @@
  * @return {ListNode}
  */
 
-var partition = function(head, x) {
-  let before = new ListNode(null);
-  let beforeHead = before;
-  let after = new ListNode(null);
-  let afterHead = after;
-
-  let read = head;
-  while (read !== null) {
-    let next = read.next;
-    read.next = null;
-    if (read.val >= x) {
-      after.next = read;
-      after = read;
+const partition = function(node, x) {
+  let head = node;
+  let tail = node;
+  while (node !== null) {
+    let next = node.next;
+    if (node.val < x) {
+      // add to head
+      node.next = head;
+      head = node;
     } else {
-      before.next = read;
-      before = read;
+      // add to tail
+      node.next = null;
+      tail.next = node;
+      tail = node;
     }
-    read = next;
+    node = next;
   }
-  before.next = afterHead.next;
-  return beforeHead.next;
+  return head;
 };
+
+// var partition = function(head, x) {
+//   let before = new ListNode(null);
+//   let beforeHead = before;
+//   let after = new ListNode(null);
+//   let afterHead = after;
+
+//   let read = head;
+//   while (read !== null) {
+//     let next = read.next;
+//     read.next = null;
+//     if (read.val >= x) {
+//       after.next = read;
+//       after = read;
+//     } else {
+//       before.next = read;
+//       before = read;
+//     }
+//     read = next;
+//   }
+//   before.next = afterHead.next;
+//   return beforeHead.next;
+// };
